2b9c3443d51414d0e6edac0131c1544b75a30f9b
Hene Chen <henecastro@gmail.com>
2022-11-15 11:03:20 -0300

    Faça um programa que exibe a soma e a subtração de 2 números (variáveis declaradas).

---
+++ src/Exercicios/Exercicio3.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+        // Faça um programa que exibe a soma e a substração de 2 números (variáveis declaradas).
+
+        int n1 = 10;
+        int n2 = 30;
+
+        int soma = n1 + n2;
+        int subtracao = n1 - n2;
+
+        System.out.println("Soma: " + soma + ", subtração: " + subtracao);
+    }
+}
