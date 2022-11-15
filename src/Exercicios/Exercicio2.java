34cb184746c237ddfee2f22f4789b334dbaa3f78
Hene Chen <henecastro@gmail.com>
2022-11-15 11:03:01 -0300

    Faça um programa que exibe uma frase concatenada com a idade e o nome.

---
+++ src/Exercicios/Exercicio2.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        // Faça um programa que exibe uma frase concatenada com a idade e o nome de um usuário (variáveis declaradas).
+
+        String nome = "Hene";
+        int idade = 26;
+
+        System.out.println("Nome: " + nome + ", idade: " + idade);
+    }
+}
