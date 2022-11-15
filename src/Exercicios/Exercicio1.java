9ad09d78597949669c190fbc8c17f00921dfb9e8
Hene Chen <henecastro@gmail.com>
2022-11-15 11:02:45 -0300

    Faça um programa que mostre a mensagem "Alô mundo" na tela.

---
+++ src/Exercicios/Exercicio1.java
@@ -0,0 +1,9 @@
+package Exercicios;
+
+public class Exercicio1 {
+    // Faça um programa que mostre a mensagem "Alô mundo" na tela.
+
+    public static void main(String[] args) {
+        System.out.println("Alô mundo");
+    }
+}
